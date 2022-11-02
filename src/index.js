1cf4ec709367c41a3592ab28ce69757d2c02f57f
birendra8757 <computersinghaniya@gmail.com>
2022-11-02 10:47:43 +0530

    use/globalMiddlewere

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment=require('moment');
 const app = express();
 
 app.use(bodyParser.json());
@@ -16,7 +17,8 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        let date =moment().format('YYYYY-MM-DD:hh:mm:ss');
+        console.log (date+"  ,  "+req.ip+" , "+req.url);
         next();
   }
   );
