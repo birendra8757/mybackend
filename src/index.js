e2ea96b5eba4f7eabd1340b64618c8b49202c84f
birendra8757 <computersinghaniya@gmail.com>
2022-11-07 22:12:28 +0530

    assignment/done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://birendrakumar:Kumar%40123@cluster0.lmqwhdb.mongodb.net/auth02-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,45 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
+const authenticate = function(req, res, next) {
+    try{
+    let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+  let decodedToken = jwt.verify(token, "functionup-lithium");
+if (!decodedToken)
+  return res.send({ status: false, msg: "token is invalid" });
+    }catch(err){
+        res.send(err)
+    }
     next()
 }
 
 
 const authorise = function(req, res, next) {
+    try{
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "functionup-lithium");
+    let tokenId = decodedToken.userId
+    let userId = req.params.userId;
+    if(tokenId==userId){
+        next()
+    }
+    else res.send("you are not authorized")
     // comapre the logged in user's id and the id in request
-    next()
+}catch(e){res.send(e)}
+} 
+
+const usercheck =async (req,res,next)=>{
+    let userId = req.params.userId;
+    let userDetails = await userModel.findById(userId);
+  if (!userDetails)
+    return res.send({ status: false, msg: "No such user exists" });
+next()
 }
+
+module.exports.authenticate=authenticate
+module.exports.authorise= authorise
+module.exports.usercheck=usercheck
